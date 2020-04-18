85b3622afc9ec773b9bc8f39abf3cb83affac37e
tand826 <takumi.ando826@gmail.com>
2020-04-19 00:04:26 +0900

    second commit

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -19,6 +19,46 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+
+      subject = "Contact From GitHub Page - tand826"
+      body = `${name} (${email}) ${message}`
+      location.href = `mailto:takumi.ando826@gmail.com?subject='${subject}'&amp;body='${body}'`
+
+      /*
+      $.ajax({
+        url: `mailto:takumi.ando826@gmail.com?subject=${subject}&amp;body=${body}`,
+        type: "GET",
+        cache: false,
+        success: function() {
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      })
+      */
+
+      /*
       $.ajax({
         url: "contact_me.php",
         type: "POST",
@@ -57,6 +97,8 @@ $(function() {
           }, 1000);
         }
       });
+      */
+
     },
     filter: function() {
       return $(this).is(":visible");
